d7cfc0b1eba57a9a78c1697d79f669bb3fb926f2
jahidstm <jahidhasanstm@gmail.com>
2025-07-27 22:15:34 +0600

    Modify

---
+++ multiple_condition.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    'Name': ['Fahim', 'Rabby', 'Jannat', 'Jahid', 'Mim'],
+    'Age': [25, 30, 22, 24, 18],
+    'City': ['Mirpur', 'Uttara', 'Dhanmondi', 'Savar', 'Dhamrai'],
+    'CGPA': [2.5, 3.0, 2.2, 3.5, 3.8]
+}
+
+# Convert the dictionary to a DataFrame
+df = pd.DataFrame(data)
+
+print("Simple DataFrame")
+print(df)
+
+filtered = df[(df["Age"] > 22) & (df["Age"] < 30)]
+print(filtered)
