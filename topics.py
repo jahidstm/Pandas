cf861ec66270be33045daec2362267d3cd33fde5
jahidstm <jahidhasanstm@gmail.com>
2025-07-27 21:16:24 +0600

    Modify

---
+++ topics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'Jahid', 'Mim'],
+    'Age': [25, 30, 22, 24, 18],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Savar', 'Dhaka'],
+    'CGPA': [2.5, 3.0, 2.2, 3.5, 3.8]
+}
+
+# Convert the dictionary to a DataFrame
+df = pd.DataFrame(data)
+
+print("Shape")
+print(df.shape)
+
+print("Column Name")
+print(df.columns)
