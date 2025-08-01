f4eb258f0fb3aff390da499630c20ff2d1f9f471
jahidstm <jahidhasanstm@gmail.com>
2025-08-01 23:26:33 +0600

    Modify

---
+++ Sorting & Aggregation/multi_group.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'Name': ['Fahim', 'Fardin', 'Jannat', 'Jahid', 'Mim'],
+    'Age': [25, 24, 22, 24, 22],
+    'City': ['Mirpur', 'Gulshan', 'Dhanmondi', 'Savar', 'Dhamrai'],
+    'Salary': [25000, 38000, 22000, 35000, 28000]
+}
+
+# Convert the dictionary to a DataFrame
+df = pd.DataFrame(data)
+
+grouped = df.groupby(["Age","Name"])["Salary"].sum()
+print(grouped)
